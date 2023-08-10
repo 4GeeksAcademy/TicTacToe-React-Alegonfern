6fb5a6210b91a6eb39f300904f92f369a7b2fd1d
Alexis Gonzalez <94706121+alegonfern@users.noreply.github.com>
2023-08-10 21:00:48 +0000

    1erComit

---
+++ src/js/component/Posision.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Posision() {
+
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default Posision;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,38 @@
 import React from "react";
+import Anuncio from './Anuncio';
+import BotonReinicio from './BotonReinicio';
+import Posision from './Posision';
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	constructor()
+	super();
+	this.state = {
+		gameBoard:[
+			' ', ' ', ' ',
+			' ', ' ', ' ',
+			' ', ' ', ' ',
+		]
+	}	
+	
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	<>
+	<div className="container">
+		<div className="menu">
+			<h1>Juego: Gato</h1>
+			<Anuncio></Anuncio>
+			<BotonReinicio></BotonReinicio>
 		</div>
+		{this.state.gameBoard.map(function(value, i){
+			<Posision></Posision>
+		})}
+	</div>
+	
+	
+	</>
 	);
 };
 
