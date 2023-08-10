8424d42eb2eca2cfcd7733ee10acffef6d9d9465
Alexis Gonzalez <94706121+alegonfern@users.noreply.github.com>
2023-08-10 21:09:19 +0000

    asd

--- src/js/component/Posision.jsx
+++ src/js/component/Posision.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-function Posision(props) {
-
-  return (
-    <div>
-    </div>
-  );
+function Posision() {
 
   function ClickPosision (props){
     props.updateBoard(props.loc, props.turn);
     
   }
-}
+  return (
+    <div>
+    </div>
+  );
+
+};
 
 export default Posision;
